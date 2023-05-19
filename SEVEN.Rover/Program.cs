d02825871c0c69db09a1924af813723aaa69a240
Matthias Röhricht <matthiasroehricht@outlook.de>
2023-05-19 22:56:51 +0200

    Update Program.cs

--- SEVEN.Rover/Program.cs
+++ SEVEN.Rover/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t\t_______ _______ _    _ _______ __   _ ");
+            Console.WriteLine("\t\t|______ |______  \\  /  |______ | \\  |");
+            Console.WriteLine("\t\t______| |______   \\/   |______ |  \\_|");
+            Console.WriteLine("\t\t\t\t\t__________                          ");
+            Console.WriteLine("\t\t\t\t\t\\______   \\ _______  __ ___________ ");
+            Console.WriteLine("\t\t\t\t\t |       _//  _ \\  \\/ // __ \\_  __ \\");
+            Console.WriteLine("\t\t\t\t\t |    |   (  <_> )   /\\  ___/|  | \\/");
+            Console.WriteLine("\t\t\t\t\t |____|_  /\\____/ \\_/  \\___  >__|   ");
+            Console.WriteLine("\t\t\t\t\t        \\/                 \\/       ");
+            Console.WriteLine();
+            Console.WriteLine("+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+");
+            Console.WriteLine("|S|a|n|d|b|e|r|g| |E|l|e|c|t|r|i|c| |V|e|h|i|c|l|e| |E|d|e|n|       |N|e|t|w|o|r|k|");
+            Console.WriteLine("+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+");
         }
     }
 }
