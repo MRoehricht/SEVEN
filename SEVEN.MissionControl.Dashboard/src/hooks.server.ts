2f1a1517ed441359f692424e4a6dadbe2751eedd
dmkk3r <dominik.koerber@protonmail.com>
2023-08-03 11:46:52 +0200

    Docker integration + authjs and sveltekit adjustments for docker hosting

--- SEVEN.MissionControl.Dashboard/src/hooks.server.ts
+++ SEVEN.MissionControl.Dashboard/src/hooks.server.ts
@@ -1,12 +1,16 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
-import { GITHUB_ID, GITHUB_SECRET, GITHUB_SECRET_KEY } from '$env/static/private';
+import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
 export const authorization = (async ({ event, resolve }) => {
 	// Protect any routes except /login
-	if (event.url.pathname.startsWith('/') && event.url.pathname !== '/login') {
+	if (
+		event.url.pathname.startsWith('/') &&
+		event.url.pathname !== '/login' &&
+		!event.url.pathname.startsWith('/auth')
+	) {
 		const session = await event.locals.getSession();
 		if (!session) {
 			throw redirect(303, '/login');
@@ -22,9 +26,7 @@ export const authorization = (async ({ event, resolve }) => {
 // And returning a handle which gets passed to the next function
 export const handle: Handle = sequence(
 	SvelteKitAuth({
-		providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-		secret: GITHUB_SECRET_KEY,
-		trustHost: true
+		providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
 	}),
 	authorization
 );
