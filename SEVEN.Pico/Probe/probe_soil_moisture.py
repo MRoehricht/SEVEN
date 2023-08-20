978d546d26860891026c4be7fd8a0637fe46300b
Matthias Röhricht <matthiasroehricht@outlook.de>
2023-08-20 09:43:31 +0200

    Update to sending airpressure, temperature and humidity

--- SEVEN.Pico/Probe/probe_soil_moisture.py
+++ SEVEN.Pico/Probe/probe_soil_moisture.py
@@ -1,10 +1,12 @@
 from machine import Pin, I2C, ADC
 import time
 import apiclient
+import bme280
 
 myId = '7a73f8ae-0000-0000-bbbb-7ab5a00a9c1d'
 adc = ADC(Pin(27))# Create an ADC object linked to pin 27
 led = Pin("LED", Pin.OUT)
+i2c=I2C(0,sda=Pin(16), scl=Pin(17), freq=400000)    
 
 def my_map(x, in_min, in_max, out_min, out_max):
   return int((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
@@ -22,8 +24,14 @@ def loop():
       #Type = SoilMoisture = 64
            
       #values = {'ProbeId': myId, 'MeasurementItems':[{"Type":64,"Value":value_percentage}]}  
-      #print(values) 
-      json = 'http://192.168.178.4:8055/measurement/create/%7B%22ProbeId%22%3A%20%227a73f8ae-0000-0000-bbbb-7ab5a00a9c1d%22%2C%20%22MeasurementItems%22%3A%5B%20%7B%22Type%22%3A64%2C%22Value%22%3A%22' + str(value_percentage) +'%22%7D%5D%7D'
+      #print(values)
+      bme = bme280.BME280(i2c=i2c)          #Init BME280
+      values = {'probeId': myId,'temperature': bme.temperature, 'airpressure' : bme.airpressure, 'humidity': bme.humidity} 
+      
+      
+      #json = 'http://192.168.178.4:5009/measurement/create/%7B%22ProbeId%22%3A%20%227a73f8ae-0000-0000-bbbb-7ab5a00a9c1d%22%2C%20%22MeasurementItems%22%3A%5B%20%7B%22Type%22%3A64%2C%22Value%22%3A%22' + str(value_percentage) +'%22%7D%5D%7D'
+      json = 'http://192.168.178.4:5009/measurement/create/%7B%22ProbeId%22%3A%20%227a73f8ae-0000-0000-bbbb-7ab5a00a9c1d%22%2C%20%22MeasurementItems%22%3A%5B%20%7B%22Type%22%3A64%2C%22Value%22%3A%22' + str(value_percentage) +'%22%7D%2C%20%7B%22Type%22%3A1%2C%22Value%22%3A%22' + str(bme.temperature) +'%22%7D%2C%20%7B%22Type%22%3A8%2C%22Value%22%3A%22' + str(bme.humidity) +'%22%7D%2C%7B%22Type%22%3A256%2C%22Value%22%3A%22' + str(bme.airpressure) +'%22%7D%5D%7D'
+                
                 
       status_code = apiclient.sendJSON(json)
       print(status_code)
